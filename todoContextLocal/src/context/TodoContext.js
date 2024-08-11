f6069de05935fe77c3d77b558282399c99e0e2b1
unknown <patil.ratnesh1997@gmail.com>
2024-08-11 14:26:52 +0530

    context project

---
+++ todoContextLocal/src/context/TodoContext.js
@@ -0,0 +1,20 @@
+import { createContext ,useContext } from "react";
+
+export const TodoContext = createContext({
+    todos : [{
+        id : 1,
+        todo : "task 1",
+        completed : false
+    }],
+    addTodo : (todo) => {},
+    updateTodo : (id,todo) => {},
+    deletedTodo : (id) => {},
+    toggleCompleteTodo : (id) => {}
+
+})
+
+export const useTodo = () => {
+ return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
